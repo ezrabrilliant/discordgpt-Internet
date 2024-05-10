9b01cbbfa44b314c3cfc2e3186c51f3b8997d861
Ezra Brilliant <64943463+ezrabrilliant@users.noreply.github.com>
2024-05-10 21:54:51 +0700

    Update handleMessage.js

--- handleMessage.js
+++ handleMessage.js
@@ -1,7 +1,7 @@
 const searchGoogle = require('./searchGoogle');
 const { Configuration, OpenAIApi } = require('openai');
-require('dotenv').config();
-
+const searchImages = require('./searchBrave');
+const { EmbedBuilder } = require('discord.js');
 
 const configuration = new Configuration({
     apiKey: process.env.API_KEY,
@@ -10,7 +10,73 @@ const openai = new OpenAIApi(configuration);
 
 async function handleMessage(message) {
     const prefgpt = ['zra', 'ezra'];
-    
+
+    const prefix = '!';
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if(command === 'image'){
+        const query = args.join(' ');
+
+        if (!query) {
+            message.reply('error no query has been input.');
+            return;
+        }
+        let result = await searchImages(query, 4, "strict");
+        if (result.length === 0) {
+            if (message.guild && message.guild.id === '1016684763752452166') 
+                await message.reply('Error : Tidak bisa melakukan koneksi dengan database BAKA.'); 
+            else 
+                await message.reply('Maaf, gak nemu data yang sesuai.');
+            return;
+        } else {
+            const embeds = [];
+            for (let i = 0; i < result.length; i++) {
+                embeds.push(
+                    new EmbedBuilder()
+                        .setURL("https://a.org/")
+                        .setImage(`${result[i]}`)
+                        .setAuthor({ name: 'Ezra Bot', iconURL: 'https://cdn.discordapp.com/avatars/1127105994766426122/e10f495c1d617ab94163c69b507e5399.png?size=4096'})
+                        .setDescription(`you've been searching\n\`\`\`${query}\`\`\``)
+                        .setFooter({ text: "Ezra Bot" })
+                );
+            }
+            message.reply({
+                content: 'Ezra has finished searching.',
+                embeds: embeds,
+            });
+        }
+    }
+
+    if(command === 'search'){
+        const query = args.join(' ');
+
+        if (!query) {
+            message.reply('error no query has been input.');
+            return;
+        }
+        
+        const results = await searchGoogle(query);
+
+        if (results.length === 0) {
+            await message.reply('Tidak ada hasil yang ditemukan.');
+            return;
+        }
+
+        const result = results[0];
+
+        let combinedResults = "";
+        combinedResults += "Source:\n";
+        combinedResults += "Title: " + result.title + "\n";
+        combinedResults += "URL: " + result.link + "\n";
+        combinedResults += "Content: " + result.snippet + "\n";
+
+        await message.reply(combinedResults);
+        return;
+    }
+
+
+
     const startsWithPrefgpt = prefgpt.some(pref => message.content.toLowerCase().startsWith(pref));
     if (!startsWithPrefgpt) return;
     if (message.author.bot) return;
