a46347c27077e5707ef4540b677969c24717dd88
Ezra Brilliant <64943463+ezrabrilliant@users.noreply.github.com>
2024-05-10 21:55:19 +0700

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,10 @@ require('dotenv').config();
 const bot = require('./botConfig');
 const handleMessage = require('./handleMessage');
 
+bot.on('ready', () => {
+    console.log(`Logged in as ${bot.user.tag}!`);
+});
+
 bot.on('messageCreate', handleMessage);
 
 bot.login(process.env.TOKEN);
